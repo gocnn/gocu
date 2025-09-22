8549bba4f32cab6d2ed82be7c85b3fea84a73a27
CtrlX <gitctrlx@gmail.com>
2025-09-22 15:41:32 +0800

    dev

---
+++ cudart/cgoflags.go
@@ -0,0 +1,12 @@
+package cudart
+
+// This file provides CGO flags to find CUDA libraries and headers.
+
+//#cgo LDFLAGS:-lcuda -lcudart
+//
+//#cgo linux LDFLAGS:-L/usr/local/cuda/lib64
+//#cgo linux CFLAGS: -I/usr/local/cuda/include
+//
+//#cgo windows LDFLAGS: -LC:/cuda/v12.0/lib/x64
+//#cgo windows CFLAGS: -IC:/cuda/v12.0/include
+import "C"
--- cudart/device.go
+++ cudart/device.go
@@ -1,1 +1,13 @@
 package cudart
+
+// #include <cuda_runtime.h>
+import "C"
+
+// Device represents a CUDA device ordinal.
+type Device int
+
+// Set the device as current.
+func SetDevice(device Device) error {
+	err := Result(C.cudaSetDevice(C.int(device)))
+	return err
+}
