d206610a6fa563998971fda83d0b813e9f736731
CtrlX <gitctrlx@gmail.com>
2025-09-29 08:37:49 +0800

    feat(gocu): Add some driver api about stream and event

---
+++ graph.go
@@ -0,0 +1,30 @@
+package gocu
+
+// #include <cuda.h>
+import "C"
+
+// CUGraph is a CUDA graph
+type CUGraph struct {
+	graph C.CUgraph
+}
+
+// C returns the CUGraph as its C version
+func (g CUGraph) c() C.CUgraph { return g.graph }
+
+// CUGraphCreate creates a new CUDA graph
+func CUGraphCreate(flags uint32) (CUGraph, error) {
+	var graph C.CUgraph
+	result := Check(C.cuGraphCreate(&graph, C.uint(flags)))
+	return CUGraph{graph: graph}, result
+}
+
+// cuGraphDestroy ( CUgraph hGraph )
+// Destroys a graph.
+func CUGraphDestroy(graph CUGraph) error {
+	return Check(C.cuGraphDestroy(graph.c()))
+}
+
+// Destroy destroys the graph (method version)
+func (g CUGraph) Destroy() error {
+	return Check(C.cuGraphDestroy(g.c()))
+}
